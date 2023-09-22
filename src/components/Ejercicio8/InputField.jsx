952d2032567eecf8e1629e7a0eb4162e86420071
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-22 09:44:31 -0300

    mejoro documentacion de ejercicio 8

--- src/components/Ejercicio8/InputField.jsx
+++ src/components/Ejercicio8/InputField.jsx
@@ -1,12 +1,14 @@
 import { forwardRef } from 'react';
 
 // forwardRef permite mandar un ref de un componente padre a un hijo
+// Este es un patrón más avanzado, pero solo implica envolver el componente en forwardRef
 const InputField = forwardRef((props, ref) => {
-  // type = "text" es el valor por defecto si no se envia ese prop
+  // type = "text" es un valor por defecto, si no se envia ese prop
   // ...other son todos los props que no se estan destructurando
   const { label, name, type = 'text', ...other } = props;
 
-  //  Workaround para montar los refs (?)
+  // Workaround para montar los refs (?)
+  // https://stackoverflow.com/questions/59522496/ref-from-react-forwardref-is-null
   if (!ref) return <></>;
 
   return (
@@ -19,15 +21,18 @@ const InputField = forwardRef((props, ref) => {
         id={`${name}-input`}
         className='form-control'
         required
+        // paso el ref al input, para poder obtener luego su valor
         ref={ref}
-        // agrego al elemento input todos los demás props
+        // agrego al elemento input todos los demás props, si es que hay
         {...other}
       />
-      <p className='invalid-feedback'>Revisa este campo</p>
+      <p className='invalid-feedback'>Revisá este campo</p>
     </fieldset>
   );
 });
 
+// Esto es necesario para que el componente se muestre bien en React DevTools
+// eslint da error si no lo agregamos, pero es basicamente ponerle el mismo nombre que el componente
 InputField.displayName = 'InputField';
 
 export default InputField;
--- src/pages/Ejercicio8.jsx
+++ src/pages/Ejercicio8.jsx
@@ -1,50 +1,70 @@
 import { useRef } from 'react';
+
 import InputField from '../components/Ejercicio8/InputField';
 
+// Este es un ejemplo IDEAL del ejercicio.
+// Esto significa que tiene muchas cosas avanzadas que pueden
+// no ocurrirles a todos los alumnos.
 const Ejercicio8 = () => {
+  // Utilizo refs para componentes no controlados
   const nameInput = useRef();
   const lastNameInput = useRef();
   const dniInput = useRef();
   const emailInput = useRef();
 
   const validateForm = () => {
-    const errors = {};
+    // Arranco suponiendo que no hay errores
+    let hasError = false;
 
+    // Las validaciones son igual que siempre, y varían según
+    // el campo que se esté validando.
+
+    // Las validaciones también podrían moverse a un archivo
+    // aparte, para que puedan ser reutilizadas más adelante en
+    // otros formularios.
+
+    // Sin embargo, para simplificar el ejercicio, las dejamos
+    // acá.
+
+    // Nombre
     const name = nameInput.current?.value;
     if (!name || name.trim().length < 3) {
-      errors['name'] = true;
+      hasError = true;
       nameInput.current?.classList.add('is-invalid');
     } else {
       emailInput.current?.classList.remove('is-invalid');
     }
 
+    // Apellido
     const lastName = lastNameInput.current?.value;
     if (!lastName || lastName.trim().length < 3) {
-      errors['lastName'] = true;
+      hasError = true;
       lastNameInput.current?.classList.add('is-invalid');
     } else {
       lastNameInput.current?.classList.remove('is-invalid');
     }
 
+    // DNI
     const dni = dniInput.current?.value;
     if (!dni || dni.trim().length < 7 || dni.trim().length > 8) {
-      errors['dni'] = true;
+      hasError = true;
       dniInput.current?.classList.add('is-invalid');
     } else {
       dniInput.current?.classList.remove('is-invalid');
     }
 
+    // Email
     const email = emailInput.current?.value;
     const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     if (!email || !emailRegex.test(email)) {
-      errors['email'] = true;
+      hasError = true;
       emailInput.current?.classList.add('is-invalid');
     } else {
       emailInput.current?.classList.remove('is-invalid');
     }
 
-    // Si el objeto errors tiene alguna propiedad, significa que hay errores
-    return Object.keys(errors).length === 0;
+    // Si no hay errores, devuelve true
+    return !hasError;
   };
 
   const handleSubmit = (e) => {
@@ -64,6 +84,9 @@ const Ejercicio8 = () => {
     >
       <h1 className='text-center'>Formulario</h1>
       <section className='row'>
+        {/* Utilizo un componente genérico, para que pueda ser reutilizado */}
+        {/* en otros componentes. Defino props básicas, que son las cosas que */}
+        {/* cambian entre cada elemento */}
         <InputField ref={nameInput} label='Nombre' name='name' maxLength='20' />
         <InputField
           ref={lastNameInput}
@@ -75,11 +98,11 @@ const Ejercicio8 = () => {
           ref={dniInput}
           label='DNI'
           name='dni'
-          type="number"
+          type='number'
           max='99999999'
           min='1000000'
         />
-        <InputField ref={emailInput} label='Email' name='email' type="email" />
+        <InputField ref={emailInput} label='Email' name='email' type='email' />
       </section>
       <div className='text-end'>
         <button type='submit' className='btn btn-primary'>
