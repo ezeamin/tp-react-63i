d72d51fd95cb1248dfc02f17275d2fa2291b95b8
Gabriel Azubel <gabrielazubel63t@gmail.com>
2023-10-02 23:46:57 -0300

    modifico ejercicio 8v2 utilizando componentes

--- src/pages/Ejercicio8v2.jsx
+++ src/pages/Ejercicio8v2.jsx
@@ -1,4 +1,5 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
+import InputFieldv2 from "../components/Ejercicio8v2/InputFieldv2";
 
 const Ejercicio8v2 = () => {
   const {
@@ -11,105 +12,66 @@ const Ejercicio8v2 = () => {
   const onSubmit = (usuario) => {
     console.log(usuario);
     reset();
-    alert('Felicidades! Podés llenar formularios!');
+    alert("Felicidades! Podés llenar formularios!");
   };
 
   return (
     <>
       <form
-        className='bg-light text-dark p-3 rounded w-100'
+        className="bg-light text-dark p-3 rounded w-100"
         onSubmit={handleSubmit(onSubmit)}
         noValidate
       >
-        <h1 className='text-center'>Formulario</h1>
-        <section className='row'>
-          <fieldset className='col-12 col-md-6 mb-2'>
-            <label htmlFor='name-input' className='form-label'>
-              Nombre
-            </label>
-            <input
-              type='text'
-              id='name-input'
-              className={`form-control ${errors.name ? 'is-invalid' : ''}`}
-              {...register('name', {
-                required: 'Campo requerido.',
-                minLength: {
-                  value: 3,
-                  message: 'Este campo tiene un mínimo de 3 caracteres.',
-                },
-                maxLength: {
-                  value: 20,
-                  message: 'Este campo tiene un máximo de 20 caracteres.',
-                },
-              })}
-            />
-            <p className='text-danger'>{errors.name?.message}</p>
-          </fieldset>
-          <fieldset className='col-12 col-md-6 mb-2'>
-            <label htmlFor='lastName-input' className='form-label'>
-              Apellido
-            </label>
-            <input
-              type='text'
-              id='lastName-input'
-              className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
-              {...register('lastName', {
-                required: 'Campo requerido.',
-                minLength: {
-                  value: 3,
-                  message: 'Este campo tiene un mínimo de 3 caracteres.',
-                },
-                maxLength: {
-                  value: 15,
-                  message: 'Este campo tiene un máximo de 15 caracteres.',
-                },
-              })}
-            />
-            <p className='text-danger'>{errors.lastName?.message}</p>
-          </fieldset>
-          <fieldset className='col-12 col-md-6 mb-2'>
-            <label htmlFor='dni-input' className='form-label'>
-              DNI
-            </label>
-            <input
-              type='number'
-              id='dni-input'
-              className={`form-control ${errors.dni ? 'is-invalid' : ''}`}
-              {...register('dni', {
-                required: 'Campo requerido.',
-                minLength: {
-                  value: 7,
-                  message: 'Este campo tiene un mínimo de 7 caracteres.',
-                },
-                maxLength: {
-                  value: 8,
-                  message: 'Este campo tiene un máximo de 15 caracteres.',
-                },
-              })}
-            />
-            <p className='text-danger'>{errors.dni?.message}</p>
-          </fieldset>
-          <fieldset className='col-12 col-md-6 mb-2'>
-            <label htmlFor='email-input' className='form-label'>
-              Email
-            </label>
-            <input
-              type='email'
-              id='email-input'
-              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              {...register('email', {
-                required: 'Campo requerido.',
-                pattern: {
-                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                  message: 'Debe ingresar un mail válido.',
-                },
-              })}
-            />
-            <p className='text-danger'>{errors.email?.message}</p>
-          </fieldset>
+        <h1 className="text-center">Formulario</h1>
+        <section className="row">
+          <InputFieldv2 type={"text"} register={register} name={"nombre"} errors={errors}
+            others={{
+              minLength: {
+                value: 3,
+                message: "Este campo tiene un mínimo de 3 caracteres.",
+              },
+              maxLength: {
+                value: 20,
+                message: "Este campo tiene un máximo de 20 caracteres.",
+              },
+            }}
+          ></InputFieldv2>
+          <InputFieldv2
+            type={"text"} register={register} name={"apellido"} errors={errors}
+            others={{
+              minLength: {
+                value: 3,
+                message: "Este campo tiene un mínimo de 3 caracteres.",
+              },
+              maxLength: {
+                value: 20,
+                message: "Este campo tiene un máximo de 20 caracteres.",
+              },
+            }}
+          ></InputFieldv2>
+          <InputFieldv2 type={"number"} register={register} name={"DNI"} errors={errors}
+            others={{
+              min: {
+                value: 1000000,
+                message: "Su DNI tiene que tener 7 cifras o más",
+              },
+              max: {
+                value: 99999999,
+                message: "Su DNI no puede excederse de las 8 cifras.",
+              },
+            }}
+          ></InputFieldv2>
+          <InputFieldv2 type={"email"} register={register} name={"email"} errors={errors}
+            others={{
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: "Debe ingresar un mail válido.",
+              },
+            }}
+          ></InputFieldv2>
         </section>
-        <div className='text-end'>
-          <button type='submit' className='btn btn-primary'>
+        <div className="text-end">
+          <button type="submit" className="btn btn-primary">
             Enviar
           </button>
         </div>
