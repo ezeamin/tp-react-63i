84afcdabd22c80a298dbf1187f121e782d4c3e23
Gabriel Azubel <gabrielazubel63t@gmail.com>
2023-10-03 08:35:21 -0300

    Update InputFieldv2.jsx
    Corrijo sintaxis de props enviados como string

--- src/components/Ejercicio8v2/InputFieldv2.jsx
+++ src/components/Ejercicio8v2/InputFieldv2.jsx
@@ -4,18 +4,18 @@ const InputFieldv2 = ({type, register, errors, name, others}) => {
     return (
         <fieldset className='col-12 col-md-6 mb-2'>
             <label htmlFor={`${name}-input`} className='form-label'>
-            {`${name.charAt(0).toUpperCase() + name.slice(1)}`}
+            {name.charAt(0).toUpperCase() + name.slice(1)}
             </label>
             <input
-              type={`${type}`}
+              type={type}
               id={`${name}-input`}
-              className={`form-control ${errors[`${name}`] ? 'is-invalid' : ''}`}
-              {...register(`${name}`,{
+              className={`form-control ${errors[name] ? 'is-invalid' : ''}`}
+              {...register(name,{
                 required: 'Campo requerido.',
                 ...others
               })}
             />
-            <p className='text-danger'>{errors[`${name}`]?.message}</p>
+            <p className='text-danger'>{errors[name]?.message}</p>
           </fieldset>
     );
 };
